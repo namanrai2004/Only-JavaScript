b054cc19a2f29602489555add0b03247e28c05c1
Naman Rai <namanrai567484@gmail.com>
2024-12-06 20:29:31 +0530

    learn lexical scoping and closure

--- practice-1.js
+++ practice-1.js
@@ -1,1 +1,25 @@
-console.log("Naman Rai");
+// console.log("Naman Rai");
+
+// Lexical Scoping.
+
+// const name = () =>{
+//     let name = "naman"
+//     const innerName = () =>{
+//         let ano = "piu"
+//         console.log(name);
+//     }
+
+//     const anotherinner = () =>{
+//             console.log(ano);
+            
+//     }
+//     anotherinner()
+//     innerName()
+// }
+// name()
+// console.log(name);
+
+//Closure
+
+
+
