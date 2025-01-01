28c22da10c23dcb6831a784b956fb19e411ba0b6
Naman Rai <namanrai567484@gmail.com>
2025-01-01 12:05:31 +0530

    add

---
+++ loops.js
@@ -0,0 +1,78 @@
+//Loops
+// let ans = 1
+// for(let i = 0; i<=10; i++){
+//     ans = ans+i
+// }
+// console.log(ans);
+
+// //callback functions
+// // const square = (n) =>{
+// //     return n*n
+// // }
+// // const cube = (n) =>{
+// //     return n*n*n
+// // }
+
+// // let a = 2, b = 3
+// // let result = square(a,b)
+// // console.log(result);
+
+// // const square = (n) =>{
+// //     return n*n
+// // }
+
+// const cube = (n,operation) =>{
+//     let result = square(a,b)
+//     console.log(result);
+//     return n*n*n
+// }
+// console.log(2,cube);
+
+
+// map/filter
+// const mul = (i) =>{
+//     return i*2
+// }
+// let arr = [1,2,3,4,5]
+
+// // const newarr = arr.map((val) =>{
+// //     return val
+// // })
+// // console.log(newarr);
+// const newarr =arr.map(mul)
+// console.log(newarr);
+
+// let arr = [1,2,3,4,5]
+// const fil = arr.filter(val =>{
+//     return val%2==0
+// })
+// console.log(fil);
+
+//Async / promise
+
+const set = setTimeout(() => {
+        let a= 2+6
+        console.log(a);
+        
+}, 2000);
+
+let a= 2*2
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,14 @@
-...obj,...obj1
+nst square = (n) =>{
+//     return n*n
+// }
+// const cube = (n) =>{
+//     return n*n*n
+// }
+
+// let a = 2, b = 3
+// let result = square(a,b)
+// console.log(result);
+
+// const square = (n) =>{
+//     return n*n
+// }
