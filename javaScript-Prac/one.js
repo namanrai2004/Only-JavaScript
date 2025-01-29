c332b596832b679b6e54cc0025bbd171776246fd
Naman Rai <namanrai567484@gmail.com>
2025-01-30 00:59:24 +0530

    some pract

---
+++ javaScript-Prac/one.js
@@ -0,0 +1,43 @@
+// let arr = [1,2,3,4]
+
+// const callback = (array,cb)=>{
+//     for(let i=0; i<=arr.length;i++){
+//         cb(array[i])
+//     }
+// }
+// callback(arr)
+
+// let arr = [1,2,3,4,5]
+// let arr1 = [1,2,3,4,5,5,6,7,7,[2,4,5],22[2,3]]
+// console.log(arr1.flat(Infinity));/
+
+// console.log([...arr,...arr1]);
+
+// console.log(arr.at(3));
+
+// const arr = [0,1,2,3,4,2,3,4,6,undefined,7,66,7,4]
+// console.log(arr[arr.length -1]);
+
+// const obj = {
+//     name: "naman",
+//     sirname: "rai"
+// }
+const obj1 = {
+    name: "neha",
+    sirname: "rai",
+    nation:{
+        contry: "indian"
+    }
+}
+
+// // let final = Object.assign(obj,obj1)
+// console.log({...obj,...obj1});
+// console.log(Object.values(obj1));
+
+
+
+
+
+
+
+
---
+++ javaScript-Prac/two.js
@@ -0,0 +1,109 @@
+// let a = true
+// let b = false
+// console.log(a && b);
+
+// let age = true
+// function ageInfo(){
+//     let age = 20
+//     return `This person is ${age} yr old`
+// }
+// function ageInfo1(){
+//     let age = 30
+//     return `This person is ${age} yr old`
+// }
+// age? console.log(ageInfo1())
+
+// const name = "naman"
+// const age = 20
+
+// const obj = {
+//     name,
+//     age
+// }
+// console.log(obj);
+
+// const salary = {
+//     first: 20000,
+//     second: 30000
+// }
+// let {first,second} = salary
+// let final = salary
+// let final1 = salary
+
+// console.log(final,final1);
+
+// function sum(a,b=5) {
+//     return a+b
+// }
+// let result  = sum(2)
+// console.log(result);
+
+// function name(...a) {
+//     console.log(a);
+    
+//     return "hello"
+// }
+// console.log(name(1,2,3,4,5));
+
+// const personArr = [
+//     {
+//         name: "naman",
+//         age: 22
+//     },
+//     {
+//         name: "shikha",
+//         age: 22
+//     },
+//     {
+//         name: "neha",
+//         age: 42
+//     },
+//     {
+//         name: "rhea",
+//         age: 16
+//     },
+// ]
+// const list = personArr.map((val,index) =>{
+//     return val.name
+// })
+// console.log(list);
+
+// const l1 = personArr.filter((val) => val.age === 22)
+// console.log(l1);
+
+// let arr = [1,1,1,2,3,4,5]
+// let newarr = arr.find((val) =>{
+//     val===1
+//     return val
+// })
+// console.log(newarr);
+
+// let arr = [1,2,3]
+
+// let add = arr.reduce((acc,curr,index,arr)=>{
+//         // console.log(acc);
+//         // console.log(curr);
+//         // console.log(index);
+//         // console.log(arr);
+        
+//         return acc+curr
+// })
+// console.log(add);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
